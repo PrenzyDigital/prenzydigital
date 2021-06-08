5b9edd6b419dfc1710f862382a78107526a88ff9
PrenzyDigital <85508975+PrenzyDigital@users.noreply.github.com>
2021-06-08 20:35:08 -0300

    Add files via upload

---
+++ prenzy.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById("hamburger");
+const mainNav = document.getElementById("mainNav")
+
+hamburger.addEventListener("click", function () {
+
+  mainNav.classList.toggle("show");
+
+});
